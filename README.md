e381dc994774a5637528b3d3827130a4d859595f
Nick Desaulniers <ndesaulniers@mozilla.com>
2014-09-03 18:00:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+#JavaScript Unicode Obfuscator
+Just a quick toy to obfuscate JS into unicode characters.
+
+##Usage
+```
+npm install -g javascript-unicode-obfuscator
+javascript-unicode-obfuscator my.js
+```
+
+##Example
+```javascript
+alert('hello world!');
+```
+becomes
+```javascript
+\u0061\u006c\u0065\u0072\u0074('\x68\x65\x6c\x6c\x6f\x20\x77\x6f\x72\x6c\x64\x21');
+```
+which you then can run within your browser.
+
+##Idea
+Not sure exactly what the rules are for what's valid JS unicode and what's not.
+From playing around, it seems like a few chars cannot be converted to `\uXXXX`
+form such as open paren, close paren, single quote, semi colon, and return.
+Within a string, chars can be converted to `\xXX` form.
+
---
+++ bin/bin.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+var fs = require('fs');
+
+var input = fs.readFileSync(process.argv[2], { encoding: 'utf8' });
+
+var output = '';
+
+function lPad (input) {
+  for (var j = input.length; j < 4; ++j) {
+    input = '0' + input;
+  }
+  return input;
+};
+
+var inString = false;
+
+for (var i = 0; i < input.length; ++i) {
+  var unicode = input[i].charCodeAt().toString(16);
+  if (unicode == 27) {
+    inString = !inString;
+    output += input[i];
+  } else if (unicode == 28 || unicode == 29 || unicode == 27 ||
+             unicode === '3b' || unicode == 'a') {
+    // ( ) ' ; <return>
+    output += input[i];
+  } else if (inString) {
+    output += '\\x' + unicode;
+  } else {
+    output += '\\u' + lPad(unicode);
+  }
+}
+
+console.log(output);
+
